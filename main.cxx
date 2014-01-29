81a755f472724ce351c91edbea59b21133b86871
slavik81 <slavik81@gmail.com>
2014-01-29 01:21:41 -0700

    Replaced Menger sponge unit with Menger sponge.

--- main.cxx
+++ main.cxx
@@ -45,71 +45,120 @@ void draw_cube() {
   glEnd();
 }
 
-void draw_unit() {
+typedef void(*draw_fn)();
+
+// draws the Menger sponge pattern using blocks drawn 
+// by the draw function. The size of each block is
+// specified by s.
+void draw_pattern(draw_fn draw, float s) {
   // move to bottom corner and draw ring
-  glTranslatef(-1, -1, -1);
-  draw_cube();
-  glTranslatef(1, 0, 0);
-  draw_cube();
-  glTranslatef(1, 0, 0);
-  draw_cube();
-  glTranslatef(0, 1, 0);
-  draw_cube();
-  glTranslatef(0, 1, 0);
-  draw_cube();
-  glTranslatef(-1, 0, 0);
-  draw_cube();
-  glTranslatef(-1, 0, 0);
-  draw_cube();
-  glTranslatef(0, -1, 0);
-  draw_cube();
+  glTranslatef(-s, -s, -s);
+  draw();
+  glTranslatef(s, 0, 0);
+  draw();
+  glTranslatef(s, 0, 0);
+  draw();
+  glTranslatef(0, s, 0);
+  draw();
+  glTranslatef(0, s, 0);
+  draw();
+  glTranslatef(-s, 0, 0);
+  draw();
+  glTranslatef(-s, 0, 0);
+  draw();
+  glTranslatef(0, -s, 0);
+  draw();
 
   // move to next row and draw ring with holes
-  glTranslatef(0, -1, 1);
-  draw_cube();
+  glTranslatef(0, -s, s);
+  draw();
 
-  glTranslatef(1, 0, 0);
-//  draw_cube();
-  glTranslatef(1, 0, 0);
-  draw_cube();
-  glTranslatef(0, 1, 0);
-//  draw_cube();
-  glTranslatef(0, 1, 0);
-  draw_cube();
-  glTranslatef(-1, 0, 0);
-//  draw_cube();
-  glTranslatef(-1, 0, 0);
-  draw_cube();
-  glTranslatef(0, -1, 0);
-//  draw_cube();
+  glTranslatef(s, 0, 0);
+//  draw();
+  glTranslatef(s, 0, 0);
+  draw();
+  glTranslatef(0, s, 0);
+//  draw();
+  glTranslatef(0, s, 0);
+  draw();
+  glTranslatef(-s, 0, 0);
+//  draw();
+  glTranslatef(-s, 0, 0);
+  draw();
+  glTranslatef(0, -s, 0);
+//  draw();
   
   // move to next row and draw ring again
-  glTranslatef(0, -1, 1);
-  draw_cube();
+  glTranslatef(0, -s, s);
+  draw();
 
-  glTranslatef(1, 0, 0);
-  draw_cube();
-  glTranslatef(1, 0, 0);
-  draw_cube();
-  glTranslatef(0, 1, 0);
-  draw_cube();
-  glTranslatef(0, 1, 0);
-  draw_cube();
-  glTranslatef(-1, 0, 0);
-  draw_cube();
-  glTranslatef(-1, 0, 0);
-  draw_cube();
-  glTranslatef(0, -1, 0);
-  draw_cube();
+  glTranslatef(s, 0, 0);
+  draw();
+  glTranslatef(s, 0, 0);
+  draw();
+  glTranslatef(0, s, 0);
+  draw();
+  glTranslatef(0, s, 0);
+  draw();
+  glTranslatef(-s, 0, 0);
+  draw();
+  glTranslatef(-s, 0, 0);
+  draw();
+  glTranslatef(0, -s, 0);
+  draw();
 
   // move back to center
-  glTranslatef(1, 0, -1);
+  glTranslatef(s, 0, -s);
+}
+
+void draw_unit() {
+  draw_pattern(draw_cube, 1);
+}
+
+const unsigned MAX_DEPTH = 4u;
+
+unsigned bound_unsigned(unsigned min, unsigned value, unsigned max) {
+  if (value > max) {
+    value = max;
+  } else if (value < min) {
+    value = min;
+  }
+  return value;
+}
+
+float power_fu(float x, unsigned y) {
+  float value = 1.0;
+  for (unsigned i=0; i<y; ++i) {
+    value *= x;
+  }
+  return value;
+}
+
+// global state for draw_pattern_recursive
+unsigned g_current_depth;
+
+// a tricky recursiive function used to supply draw_pattern back to itself.
+// to match the signature of draw_fn, the scale is based on g_current_depth.
+void draw_pattern_recursive() {
+  unsigned frame_depth = g_current_depth;
+  if (frame_depth > 0u) {
+    --g_current_depth;
+    draw_pattern(draw_pattern_recursive, power_fu(3.0, frame_depth));
+    ++g_current_depth;
+  } else {
+    draw_pattern(draw_cube, 1);
+  }
+}
+
+void draw_menger_sponge(unsigned depth) {
+  g_current_depth = bound_unsigned(0, depth, MAX_DEPTH);
+  draw_pattern_recursive();
 }
 
 void display() {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  draw_unit();
+  draw_menger_sponge(2);
 
   glutSwapBuffers();
 }
@@ -122,7 +171,7 @@ void init() {
   glLoadIdentity();
   glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
   glTranslatef(0.5, 0.5, 0.5);
-  glScalef(1.0/8, 1.0/8, 1.0/8);
+  glScalef(1.0/64, 1.0/64, 1.0/64);
   glRotatef(30.0, 1.0, 1.0, 1.0);
 }
 
@@ -147,7 +196,7 @@ int main(int argc, char** argv) {
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGB);
   glutInitWindowSize(480, 480); 
   glutInitWindowPosition(100, 100);
-  glutCreateWindow ("RAWR!");
+  glutCreateWindow ("C Menger Sponge");
   init();
   display(); 
   glutDisplayFunc(display);
